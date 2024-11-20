98188cd37d276e140061b0fb2060b893f2d0c419
Akaki <akakiburjanadze17@gmail.com>
2024-11-20 18:44:32 +0400

    Add CaseConversion.java

---
+++ CaseConversion.java
@@ -0,0 +1,13 @@
+public class CaseConversion {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+
+        // convert to lowercase
+        String lowerStr = str.toLowerCase();
+        // convert to uppercase
+        String upperStr = str.toUpperCase();
+
+        System.out.println(lowerStr);
+        System.out.println(upperStr);
+    }
+}
