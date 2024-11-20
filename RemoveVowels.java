8a74c981fb3f85cd5f5d617cc5429cfa05b5d976
Akaki <akakiburjanadze17@gmail.com>
2024-11-20 17:43:42 +0400

    Add comments to explain specific lines of code

--- RemoveVowels.java
+++ RemoveVowels.java
@@ -8,19 +8,26 @@ public class RemoveVowels {
     }
 
     public static String removeVowels(String str) {
+        // create a StringBuilder object where we will store the string without vowels
         StringBuilder result = new StringBuilder();
 
+        // iterate through the whole string
         for (int i = 0; i < str.length(); i++) {
+            // grab character at the current position
             char c = str.charAt(i);
+            // check if it is vowel and in that case just skip it
             if (isVowel(c)) {
                 continue;
             }
+            // otherwise append it to the StringBuilder object
             result.append(c);
         }
 
+        // convert StringBuilder back into a string and return it
         return result.toString();
     }
 
+    // this method checks wheter a given character is a vowel or not
     public static boolean isVowel(char c) {
         char lowerChar = Character.toLowerCase(c);
 
